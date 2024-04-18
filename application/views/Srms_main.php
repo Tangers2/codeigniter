c65bac5d1bf9cff49fd57accb6c5c444007a115e
Clarissa B. Vega <issayvega@gmail.com>
2024-04-18 22:19:59 +0800

    try

--- application/views/Srms_main.php
+++ application/views/Srms_main.php
@@ -29,11 +29,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		echo '<td>'.$student['lname'].'</td>';
 		echo '<td>'.$student['fname'].'</td>';
 		echo '<td>'.$student['mname'].'</td>';
-		echo '<td align="center">'.'<a href="edit.php?id= $student->$student_id"
-		class="btn btn-secondary"
-		" style="margin-left: 5px;">Edit</a>'.
-		'<a href="delete.php?id= $student -> stud_id" class="btn btn-danger" onclick="return confirm(\'Are you sure you want to delete this data?\')" style="margin-left: 5px;">
-		DELETE</a>'.'</td>';
+		echo '<td align="center">'.
+		'<a href="edit.php?id=' . $student->students . '" class="btn btn-secondary" style="margin-left: 5px;">Edit</a>'.
+		'<a href="del_main.php?id=' . $student->students . '" class="btn btn-danger" onclick="return confirm(\'Are you sure you want to delete this data?\')" style="margin-left: 5px;">DELETE</a>'.
+		'</td>';
+   
 
 		echo '</tr>';
 		$counter++;
--- application/views/del_main.php
+++ application/views/del_main.php
@@ -1,15 +1,7 @@
 <?php
     $stud_id = $_GET["id"];
 
-    $servername = "localhost";
-    $username = "root";
-    $password = '';
-    $db_name = 'student_db';
 
-    $conn = new mysqli($servername, $username, $password, $db_name);
-    if ($conn->connect_errno){
-        die ('Connection Failed'.$conn->error);
-    }
 
     $sql = "DELETE FROM tbl_student where stud_id='".$stud_id."'";
     $conn ->query($sql);
